b930e738ce5f6f8fb4bdebf553a0d8a20db58725
ZaneFerns360 <zane.ferns360@gmail.com>
2024-01-04 03:06:43 +0530

    Navbar Change

--- client2/src/app/_components/Navbar.tsx
+++ client2/src/app/_components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = async () => {
 
   return (
     <div>
-      <nav className="border-gray-200 bg-white dark:bg-gray-900">
+      <nav className=" border-b border-black bg-white dark:bg-gray-900">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <a
             href="#"
